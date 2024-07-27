aabd0926649f429cc843d9c6f5f0c58e8e400615
Devrim Mehmet Pattabanoğlu <devrimmehmet@gmail.com>
2024-07-27 22:51:18 +0300

    Update Design.

---
+++ Services/Task/GTSProject.Services.Task/Models/Dtos/BaseDtos/ErrorDto.cs
@@ -0,0 +1,24 @@
+﻿namespace UserManagementSystem.Models.Dtos.BaseDtos
+{
+    public class ErrorDto
+    {
+        public List<String> Errors { get; private set; } = [];
+
+        public bool IsShow { get; private set; }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+
+       
+      
+    }
+}
---
+++ Services/Task/GTSProject.Services.Task/Models/Dtos/BaseDtos/ResponseDto.cs
@@ -0,0 +1,35 @@
+﻿namespace UserManagementSystem.Models.Dtos.BaseDtos
+{
+
+    public class ResponseDto<T> where T : class
+    {
+
+        public T? Data { get; set; }
+
+        public int status { get; set; }
+
+        public bool IsSuccessful { get; set; } // kendi iç yapımızda kullanacağız.
+        public ErrorDto? errors { get; set; }
+   
+        public static ResponseDto<T> Success(T data, int status)
+        {
+            return new ResponseDto<T> { Data = data, status = status, IsSuccessful = true };
+        }
+        public static ResponseDto<T> Success(int status)
+        {
+            return new ResponseDto<T> { Data = default, status = status, IsSuccessful = true };
+        }
+        public static ResponseDto<T> Fail(ErrorDto errorDto, int status)
+        {
+            return new ResponseDto<T> { errors = errorDto, status = status, IsSuccessful = false };
+        }
+      
+        public static ResponseDto<T> Fail(string errorMessage, int status, bool isShow)
+        {
+            var errorDto = new ErrorDto(errorMessage, isShow);
+            return new ResponseDto<T> { errors = errorDto, status = status, IsSuccessful = false };
+
+        }
+    }
+
+}
