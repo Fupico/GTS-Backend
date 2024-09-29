85bf5d3112b95f7069d997fd54f740ba88200e30
ssule <ssulecelepp@gmail.com>
2024-09-29 16:49:52 +0300

    dtolar oluşturuldu ve result dönüldü.

---
+++ Services/File/GTSProject.Services.File/Dtos/RequestDtos/UploadFileRequestDtos/ResultGetAllUploadFileMongoRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GTSProject.Services.File.Dtos.RequestDtos.UploadFileRequestDtos
+{
+    public class ResultGetAllUploadFileMongoRequestDto
+    {
+        public string StorageProvider { get; set; } = "mongodb"; //mssql //mongodb
+    }
+}
---
+++ Services/File/GTSProject.Services.File/Dtos/RequestDtos/UploadFileRequestDtos/ResultGetAllUploadFileMssqlRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace GTSProject.Services.File.Dtos.RequestDtos.UploadFileRequestDtos
+{
+    public class ResultGetAllUploadFileMssqlRequestDto
+    {
+        public string StorageProvider { get; set; } = "mssql"; //mssql //mongodb
+    }
+}
